641134471fd863352c6fec487a188d403d0d1e40
Roman <rkonyushenko@softwear.finance>
2018-03-13 19:55:08 +0200

    server update

--- dbIntegration.js
+++ dbIntegration.js
@@ -5,46 +5,69 @@ const config = {
     password: 'zeng?vyv123',
     tableName: 'rabbit'
 };
- 
+
 const pool = new pg.Pool(config);
+
 class LocalDb {
-    constructor(){}
-    
- 
-    query(sql, params){
-        return new Promise((resolve, reject)=> {
-           // if(Array.isArray(params)) {
-                //if(params.length > 0) {
-                    pool.connect((err, client, done)=>{
-                        if (err) {
-                            reject(err);
-                        }
-                        client.query(sql, params)
-                            .then(result => {
-                                done();
-                                console.log(result.rows);
-                                resolve(result.rows);
-                            })
-                            .catch(err => {
-                                done();
-                                reject(new Error(err))
-                            })
-                    });
-               // }
-           // } else {
-                //reject('Error: params are NaN, checkout input parameters ' + params);
-           // }
+    constructor() {
+    }
+
+
+    query(sql, params) {
+        return new Promise((resolve, reject) => {
+            // if(Array.isArray(params)) {
+            //if(params.length > 0) {
+            pool.connect((err, client, done) => {
+                if (err) {
+                    reject(err);
+                }
+                client.query(sql, params)
+                    .then(result => {
+                        done();
+                        resolve(JSON.stringify(result.rows));
+                    })
+                    .catch(err => {
+                        done();
+                        reject(new Error(err))
+                    })
+            });
+            // }
+            // } else {
+            //reject('Error: params are NaN, checkout input parameters ' + params);
+            // }
         })
- 
+
     };
- 
-    insertData(data, type){
+
+    insertData(data, type) {
         this.query(`INSERT INTO ${config.tableName} (data, type, date) VALUES ($1, $2, $3)`, [data, type, new Date().toISOString()]);
     }
 
-    getAllLogs(){
-        this.query(`SELECT * FROM ${config.tableName}`);
+    getAllLogs() {
+        return new Promise((resolve, reject) => {
+            this.query(`SELECT * FROM ${config.tableName}`)
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
     }
+
+    getErrors() {
+        return new Promise((resolve, reject) => {
+            this.query(`select * from ${config.tableName} where type = 'error' `)
+                .then(result => {
+                    resolve(result)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+
 }
+
 const dbClient = new LocalDb();
 module.exports = dbClient;
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const db = require('./dbIntegration');
+const log = require('debug-logger')('myapp');
 const port = 3000;
 
 app.get('/', (req, res) => res.send('app is working'));
 
 app.get('/logs', (req, res) => {
-    console.log(db.getAllLogs());
-    res.send(db.getAllLogs())
+    db.getAllLogs()
+        .then(result => {
+            res.send(result)
+        })
 });
+
+app.get('/logs/errors/', (req, res) => {
+    db.getErrors()
+        .then(result => {
+            res.send(result);
+        })
+});
+
+app.get('/query', (req, res) => {
+
+});
+
 
 app.listen(port, () => console.log(`server living on ${port} port`));
